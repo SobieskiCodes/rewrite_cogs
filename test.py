8226063832d2b662b3e53ce424bdb22fe43583b1
Justin Sobieski <jmksobieski@gmail.com>
2020-02-25 14:16:43 -0500

    ignore

---
+++ test.py
@@ -0,0 +1,16 @@
+from discord.ext import commands
+
+
+class info(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def info(self, ctx):
+        '''info - see current information you might need to know about changes.
+        '''
+        await ctx.send(f"hello world")
+
+
+def setup(bot):
+    bot.add_cog(info(bot))
